6567afdc756d0fb086799b1ced44802248917efe
Will Anderson <will@itsananderson.com>
2014-07-12 13:57:55 -0700

    Add tests for inline annotated functions

--- test/index.js
+++ test/index.js
@@ -54,4 +54,20 @@ describe('funcDeps', function() {
             assert.equal(testDeps.func, test);
         });
     });
+    describe('called with inline annotated functions', function() {
+        it('handles functions with different args', function() {
+            function test(c, d) {}
+            test.$deps = ['a','b'];
+            var testDeps = funcDeps(test);
+            assert.deepEqual(testDeps.deps, ['a','b']);
+            assert.equal(testDeps.func, test);
+        });
+        it('handles functions with no args', function() {
+            function test() {}
+            test.$deps = ['a','b'];
+            var testDeps = funcDeps(test);
+            assert.deepEqual(testDeps.deps, ['a','b']);
+            assert.equal(testDeps.func, test);
+        });
+    });
 });
