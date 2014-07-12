d9734f2e7533b45467e532959d280d41469ff5d2
Will Anderson <will@itsananderson.com>
2014-07-12 13:53:52 -0700

    Flesh out tests for non-annotated functions

--- test/index.js
+++ test/index.js
@@ -3,7 +3,7 @@ var funcDeps = require('../');
 
 describe('funcDeps', function() {
     describe('called with non-annotated functions', function() {
-        it('handles parameterless', function() {
+        it('handles parameterless functions', function() {
             function test() {}
             var testDeps = funcDeps(test);
             assert.deepEqual(testDeps.deps, []);
@@ -19,6 +19,24 @@ describe('funcDeps', function() {
             var test = function(a,b) {}
             var testDeps = funcDeps(test);
             assert.deepEqual(testDeps.deps, ['a','b']);
+            assert.equal(testDeps.func, test);
+        });
+        it('handles functions with comments in parameter list', function() {
+            var test = function(a,/* test
+                we should be able to do all kinds of crazy stuff in here
+                /*
+                * /
+                , c, d
+            **/b) {}
+            var testDeps = funcDeps(test);
+            assert.deepEqual(testDeps.deps, ['a','b']);
+            assert.equal(testDeps.func, test);
+        });
+        it('handles functions with whitespace in parameter list', function() {
+            var test = function(    a , b,   c,
+            d ) {}
+            var testDeps = funcDeps(test);
+            assert.deepEqual(testDeps.deps, ['a','b','c','d']);
             assert.equal(testDeps.func, test);
         });
     });
